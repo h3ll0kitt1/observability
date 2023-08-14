eeabd99b97bcfcd01d3b0b9be4ede28bb6e73201
h3ll0kitt1 <>
2023-08-14 10:04:10 -0400

    Add sql storage as alternative to inmemory storage

--- internal/storage/sql/sql.go
+++ internal/storage/sql/sql.go
@@ -1,7 +1,11 @@
 package sql
 
 import (
+	"context"
 	"database/sql"
+	"log"
+	"time"
+
 	_ "github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/h3ll0kitt1/observability/internal/config"
@@ -9,7 +13,7 @@ import (
 )
 
 type SQLStorage struct {
-	DB *sql.DB
+	db *sql.DB
 }
 
 func NewStorage(cfg *config.ServerConfig) *SQLStorage {
@@ -17,24 +21,147 @@ func NewStorage(cfg *config.ServerConfig) *SQLStorage {
 	if err != nil {
 		return nil
 	}
-	return &SQLStorage{DB: db}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `CREATE TABLE IF NOT EXISTS counter(
+		metric_id varchar(512) primary key, 
+		metric_value bigint not null)`
+	_, err = db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating counter table", err)
+		return nil
+	}
+
+	query = `CREATE TABLE IF NOT EXISTS gauge(
+		metric_id varchar(512) primary key, 
+		metric_value double precision not null)`
+	_, err = db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating gauge table", err)
+		return nil
+	}
+
+	return &SQLStorage{db: db}
 }
 
 func (s *SQLStorage) Update(metric models.MetricsWithValue) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
+	switch metric.MType {
+	case "counter":
+		_, err := s.db.ExecContext(ctx, `INSERT INTO counter (metric_id, metric_value) 
+										VALUES ($1, $2) 
+										ON CONFLICT (metric_id)
+										DO
+										UPDATE SET metric_value = EXCLUDED.metric_value;`, metric.ID, metric.Delta)
+		if err != nil {
+			log.Printf("Error %s when inserting in counter table", err)
+		}
+	case "gauge":
+		_, err := s.db.ExecContext(ctx, `INSERT INTO gauge (metric_id, metric_value) 
+										VALUES ($1, $2) 
+										ON CONFLICT (metric_id) DO UPDATE 
+										SET metric_value = EXCLUDED.metric_value;`, metric.ID, metric.Value)
+		if err != nil {
+			log.Printf("Error %s when inserting in gauge table", err)
+		}
+	}
 }
 
 func (s *SQLStorage) GetList() []*models.MetricsWithValue {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	list := make([]*models.MetricsWithValue, 0)
+
+	rows, err := s.db.QueryContext(ctx, "SELECT metric_id, metric_value FROM counter")
+	if err != nil {
+		//return nil, err
+		return nil
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var metric models.MetricsWithValue
+		metric.MType = "counter"
+
+		err = rows.Scan(&metric.ID, &metric.Delta)
+		if err != nil {
+			//return nil, err
+			return nil
+		}
+		list = append(list, &metric)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		//return nil, err
+		return nil
+	}
+
+	rows, err = s.db.QueryContext(ctx, "SELECT metric_id, metric_value FROM gauge")
+	if err != nil {
+		//return nil, err
+		return nil
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var metric models.MetricsWithValue
+		metric.MType = "gauge"
+
+		err = rows.Scan(&metric.ID, &metric.Value)
+		if err != nil {
+			//return nil, err
+			return nil
+		}
+		list = append(list, &metric)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		//return nil, err
+		return nil
+	}
+
 	return list
 }
 
 func (s *SQLStorage) GetValue(metric models.MetricsWithValue) (models.MetricsWithValue, bool) {
-	return models.MetricsWithValue{}, false
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	switch metric.MType {
+
+	case "counter":
+		var value int64
+
+		row := s.db.QueryRowContext(ctx, "SELECT metric_value FROM counter WHERE metric_id = $1", metric.ID)
+
+		if err := row.Scan(&value); err != nil {
+			return metric, false
+		}
+		metric.Delta = value
+
+	case "gauge":
+		var value float64
+
+		row := s.db.QueryRowContext(ctx, "SELECT metric_value FROM gauge WHERE metric_id = $1", metric.ID)
+
+		if err := row.Scan(&value); err != nil {
+			return metric, false
+		}
+		metric.Value = value
+	}
+
+	return metric, true
 }
 
 func (s *SQLStorage) Ping() bool {
-	if err := s.DB.Ping(); err != nil {
+	if err := s.db.Ping(); err != nil {
 		return false
 	}
 	return true
